feaded6c57454eb3442a384b33890812fa2c4923
MitanshuVerma <mitanshu4806@gmail.com>
2026-06-19 22:04:21 +0530

    Fix navbar filename casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ForceDetails from "./pages/ForceDetails";
 import Analytics from "./pages/Analytics";
